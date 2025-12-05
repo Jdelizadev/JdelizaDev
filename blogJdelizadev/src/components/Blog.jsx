53bd0e0670e353c42d54a301a447340f825e1114
Jaime De Lizaola <jaimedelizaola@gmail.com>
2025-12-05 14:18:13 -0600

    main section

--- blogJdelizadev/src/components/Blog.jsx
+++ blogJdelizadev/src/components/Blog.jsx
@@ -1,13 +1,68 @@
 
 import './Blog.css'
+import bkCt from '../assets/fondoCt.png'
+
 
 const Blog = () => {
+
+
+    //aqui pueden ir los documentos
+
+
     return (
         <>
             <section className="blog-container">
 
-             <h1>Menu para blog</h1>
+             <p className='main-title'>Comienza a explorar</p>
 
+             <main className='articles-container'>
+             <article className='platzi-container'>
+                <div className="card-container">
+                    <div className='card-img-container'>
+                        <img src={bkCt} alt="certificado plazti" />
+                    </div>
+                    <div className='text-container'>
+                        <p className='card-title'>Galería de diplomas y certificados </p>
+                        <a className='card-subtitle' href="">Navegar</a>
+                    </div>
+                </div>
+             </article>
+
+             <article className='platzi-container'>
+                <div className="card-container">
+                    <div className='card-img-container'>
+                        <img src={bkCt} alt="certificado plazti" />
+                    </div>
+                    <div className='text-container'>
+                        <p className='card-title'>Galería de diplomas y certificados </p>
+                        <a className='card-subtitle' href="">Navegar</a>
+                    </div>
+                </div>
+             </article>
+
+            <article className='platzi-container'>
+                <div className="card-container">
+                    <div className='card-img-container'>
+                        <img src={bkCt} alt="certificado plazti" />
+                    </div>
+                    <div className='text-container'>
+                        <p className='card-title'>Galería de diplomas y certificados </p>
+                        <a className='card-subtitle' href="">Navegar</a>
+                    </div>
+                </div>
+             </article>
+             <article>
+                <div className="card">
+
+                </div>
+             </article>
+
+             <article>
+                <div className="card">
+
+                </div>
+             </article>
+            </main>
             </section>
         </>
     )
--- blogJdelizadev/src/main.jsx
+++ blogJdelizadev/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+import "@fontsource/bitcount-prop-single/400.css"; // Normal
+import "@fontsource/bitcount-prop-single/700.css"; // Bold
+
 const root = document.querySelector('#root')
 
 createRoot(root).render(
